9b9f7274f74a255db7c80b94fda6343bc5b3ace2
Alex Pedcenko <33809152+aa3025@users.noreply.github.com>
2021-03-09 23:40:13 +0000

    Update runcase.sh

--- template/runcase.sh
+++ template/runcase.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # run from case folder
 
+module load openfoam/last
+
 solver=`cat system/controlDict | grep application | tr -s " " | cut -d' ' -f2 | cut -d';' -f1 | tail -1`
 
 foamGet Allclean
-./Allclean
+sh ./Allclean
 blockMesh
 decomposePar
 
 
-cpus=`lscpu | grep "CPU(s):" | head -1 |tr -s " " | cut -d' ' -f2` # how many mpi processes to launch (e.g. Nr of CPUs we have on local the machine) 
+#cpus=`lscpu | grep "CPU(s):" | head -1 |tr -s " " | cut -d' ' -f2` # how many mpi processes to launch (e.g. Nr of CPUs we have on local the machine)
+
+cpus=$SLURM_NTASKS
 mpirun -np $cpus $solver -parallel
 
 ./reconstruct_parallel.sh
-
-touch case.foam
