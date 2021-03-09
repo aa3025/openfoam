7beb7ed4dba63687df046449f3c64f236f347a5e
Alex Pedcenko <33809152+aa3025@users.noreply.github.com>
2021-03-09 23:39:31 +0000

    Update reconstruct_parallel.sh

--- template/reconstruct_parallel.sh
+++ template/reconstruct_parallel.sh
@@ -4,6 +4,8 @@
 # It assigns uniformly all the timesteps between CPUs
 # Launch from your OpenFOAM case folder
 
+module load openfoam/last
+
 # first we get startTime, endTime and dt from the case itself:
 start=`cat system/controlDict | grep startTime | tr -s " " | cut -d' ' -f2 | cut -d';' -f1`
 stop=`cat system/controlDict | grep endTime | tr -s " " | cut -d' ' -f2 | cut -d';' -f1 | tail -1`
@@ -13,6 +15,7 @@ deltaT=`cat system/controlDict | grep deltaT | tr -s " " | cut -d' ' -f2 | cut -
 dt=$(echo "${deltaT}*${writeInterval}" |bc) # OpenFoam solution write interval [seconds], leave as decimal! (i.e. dt = deltaT * writeInterval)
 
 cpus=`lscpu | grep "CPU(s):" |tr -s " " | cut -d' ' -f2` # how many reconstructPar processes to launch (e.g. Nr of CPUs we have)
+cpus=$SLURM_NTASKS
 
 echo "Reconstructing from $start to $stop with step $dt on $cpus CPUs"
 
@@ -48,3 +51,4 @@ while true
 done
 
 # rm -fr processor* # optional if you want to clean up space
+
